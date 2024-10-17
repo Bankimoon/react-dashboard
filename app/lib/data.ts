20df2a2597d3b9779eb5009562de0c7c821de0cc
Arthur Soulheban <arthur.soulheban@atos.net>
2024-10-17 15:38:04 +0200

    dashboard loading + specific suspens

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,7 +12,7 @@ import { formatCurrency } from './utils';
 export async function fetchRevenue() {
   try {
     console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 30000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     console.log('Data fetch completed after 3 seconds.');
